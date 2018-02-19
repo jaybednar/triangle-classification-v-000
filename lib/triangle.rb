f8f7d6d87ea632e26bcaaa24690ab798f24ef29a
Jay Bednar <ironviralmedia@gmail.com>
2018-02-19 23:51:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,18 @@
+require 'pry'
+
 class Triangle
   attr_accessor :kind, :equilateral, :isosceles, :scalene, :l1, :l2, :l3
 
   def initialize(l1, l2, l3)
     @l1 = l1
     @l2 = l2
-    @l3 = l3  
+    @l3 = l3
   end
 
   def kind
+    binding.pry
     if self.l1 == self.l2 && self.l2 == self.l3
       self.kind = @equilateral 
-    elsif 
+    end
   end
 end
