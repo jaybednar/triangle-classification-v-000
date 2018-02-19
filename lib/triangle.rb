8baf0910035e20ef7e76c9bc6961d0d65a76a7f7
Jay Bednar <ironviralmedia@gmail.com>
2018-02-19 23:47:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
-  # write code here
+  attr_accessor :kind, :equilateral, :isosceles, :scalene   
+
+  def initialize(length1, length2, length3)
+    
+  end 
+
+  def kind 
+    
+  end 
 end
